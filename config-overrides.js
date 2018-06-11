a3efb673da074501ea7dbd7cb6173cff85e53f67
Steve Leach <sleach@wayfair.com>
2018-06-11 11:12:32 -0400

    fix styled components

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 /* config-overrides.js */
+const rewireStyledComponents = require("react-app-rewire-styled-components");
 
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
